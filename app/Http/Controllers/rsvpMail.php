b995e11388e5132016af41e9d46f9fe1be03f198
xschoenberger <nik.schoe@icloud.com>
2018-12-01 16:45:05 +0100

    Mail --update

--- app/Http/Controllers/rsvpMail.php
+++ app/Http/Controllers/rsvpMail.php
@@ -10,8 +10,8 @@ class rsvpMail extends Controller
     {
         $data = array("name" => $user, "rsvp" => $rsvp, "plus_one" => $plus_one);
         Mail::send("rsvp_mail", $data, function($message) {
-             $message->to('nik.schoe@gmail.com', "New Year's Eve")->subject
-                ('New RSVP Submit!');
+             $message->to('newyearseve@ineffable.at', "New Year's Eve")->cc("nik.schoe@icloud.com")->subject
+                ('New RSVP submission!');
         });
         dd('Mail Send Successfully');
     }
